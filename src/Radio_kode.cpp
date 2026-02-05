7c1f8e6e90145002ddac625a17148205203d1819
axelharr <axelv.harr@gmail.com>
2026-02-05 18:30:52 +0100

    Refactor radio functions into Radio struct in Radio_kode.cpp

--- src/Radio_kode.cpp
+++ src/Radio_kode.cpp
@@ -4,32 +4,33 @@
 
 SoftwareSerial mySerial(0, 1); // RX, TX
 
-void radio_PowerUp() {
-    mySerial.begin(38400);
-}
+struct Radio {
+    void PowerUp() {
+        mySerial.begin(38400);
+        }
 
-void radio_Setup() {
-    mySerial.write("mode ax25-1k2\r\n");
-    delay(100);
-    mySerial.write("access csma\r\n");
-    delay(100);
-    mySerial.write("freq 145500000\r\n");
-    delay(100);
-    mySerial.write("port0 text\r\n");
-    delay(100);
-    mySerial.write("set\r\n");
-}
-
-void radio_ExitRF() {
-    mySerial.write(3); //Ctrl+C
-}
-
-String radio_ReadLine() {
-    String line = "";
-    if (mySerial.available()) {
-        line = mySerial.readStringUntil('\n'); // reads whole command as string
-        line.trim(); // removes \r and starting and ending whitespaces
+    void Setup() {
+        mySerial.write("mode ax25-1k2\r\n");
+        delay(100);
+        mySerial.write("access csma\r\n");
+        delay(100);
+        mySerial.write("freq 145500000\r\n");
+        delay(100);
+        mySerial.write("port0 text\r\n");
+        delay(100);
+        mySerial.write("set\r\n");
     }
-    return line;
-}
 
+    void ExitRF() {
+        mySerial.write(3); //Ctrl+C
+    }
+
+    String ReadLine() {
+        String line = "";
+        if (mySerial.available()) {
+            line = mySerial.readStringUntil('\n'); // reads whole command as string
+            line.trim(); // removes \r and starting and ending whitespaces
+        }
+        return line;
+    }
+};
